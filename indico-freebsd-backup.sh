d8053ba1df53cf7f7beee2badc4ac474e77371f2
Graham Percival <gperciva@tarsnap.com>
2024-11-02 17:57:49 -0800

    backup: reformat for line length, bump version

--- indico-freebsd-backup.sh
+++ indico-freebsd-backup.sh
@@ -1,21 +1,26 @@
 #!/bin/sh
 
-# Backup script - version 1.0
+# Backup script - version 1.1
 
 # Get the date as epoch seconds
 DATE=$(date "+%s")
 
 # Create sql file for indico database
-
 pg_dump indico > /root/"${DATE}"-indico.sql
 
-# Make copies of WSGI config /usr/local/www/app-uwsgi (includes /usr/local/www/app-uwsgi/indico/etc/indico.conf and /usr/local/www/app-uwsgi/indico/archive file uploads/submissions )
-# celery config (in /etc/rc.conf and in the indico venv)
-# nginx config, (in /usr/local/etc/nginx)
+# Make copies of:
+# - WSGI config /usr/local/www/app-uwsgi (includes
+#   /usr/local/www/app-uwsgi/indico/etc/indico.conf
+#   and
+#   /usr/local/www/app-uwsgi/indico/archive
+#   (file uploads/submissions))
+# - celery config (in /etc/rc.conf and in the indico venv)
+# - nginx config (in /usr/local/etc/nginx)
 
-tar czvf /root/"${DATE}"-indico-files.tgz /etc/rc.conf /usr/local/www/app-uwsgi /usr/local/etc
+tar czvf /root/"${DATE}"-indico-files.tgz		\
+  /etc/rc.conf /usr/local/www/app-uwsgi /usr/local/etc
 
 
-# Note - when extracting things from the tarball - extract (without using -P) somewhere other than the / directory
-# to avoid clobbering /etc/rc.conf and anything else by accident.
-
+# Note - when extracting things from the tarball - extract (without using -P)
+# somewhere other than the / directory to avoid clobbering /etc/rc.conf and
+# anything else by accident.
